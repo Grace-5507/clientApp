66771fd5bf0f909f8e85b266cfaa1a9b775fa643
Grace-5507 <lornaolum1998@gmail.com>
2023-02-14 21:28:43 +0300

    update frontend

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api #Flask_restx provides a coherent collection of decorators and tools to describe  API and expose its documentation properly (using Swagger).
 from contacts import contacts_ns
 from clients import clients_ns
+from clients import linked_clients_ns
+from contacts import linked_contacts_ns
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from models import *
@@ -17,6 +19,8 @@ api=Api(app, doc="/docs")
 
 api.add_namespace(contacts_ns)
 api.add_namespace(clients_ns)
+api.add_namespace(linked_clients_ns)
+api.add_namespace(linked_contacts_ns)
 
 
     
